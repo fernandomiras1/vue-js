5a5a01e788265f5ea0f5a7aada4f5ae57e8fa1ae
fernandomiras1 <fernando.miras.pc@gmail.com>
2020-12-20 11:14:32 -0300

    proyecto nuevo router

---
+++ Curso-de-Vue-JS-2-en-Español/router-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+      
+      <div class="jumbotron">
+        <h1>hola</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
